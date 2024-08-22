ca686aad9829367fdc23fa2466810a3108cf3bdb
DavideGaliano <jconstantin@tiscali.it>
2024-08-22 10:01:22 +0200

    milestone 1 completata
    Creare due metodi
    “/movies/{id}”
    “/songs/{id}”
    che dato il parametro id passato tramite il path, mostri in pagina il titolo relativo al film / canzone.

--- src/main/java/org/lessons/java/controller/HomeController.java
+++ src/main/java/org/lessons/java/controller/HomeController.java
@@ -2,6 +2,7 @@ package org.lessons.java.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.lessons.java.model.Movie;
@@ -9,6 +10,7 @@ import org.lessons.java.model.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -51,5 +53,39 @@ public class HomeController {
             .collect(Collectors.toList());
         model.addAttribute("titles", String.join(", ", songTitles));
         return "songs";
+    }
+    
+    @GetMapping("/movies/{id}")
+    public String movieById(@PathVariable int id, Model model) {
+        Optional<Movie> movie = getMovieById(id);
+        if (movie.isPresent()) {
+            model.addAttribute("title", movie.get().getTitle());
+        } else {
+            model.addAttribute("title", "Movie not found");
+        }
+        return "movie-detail";
+    }
+
+    @GetMapping("/songs/{id}")
+    public String songById(@PathVariable int id, Model model) {
+        Optional<Song> song = getSongById(id);
+        if (song.isPresent()) {
+            model.addAttribute("title", song.get().getTitle());
+        } else {
+            model.addAttribute("title", "Song not found");
+        }
+        return "song-detail";
+    }
+
+    private Optional<Movie> getMovieById(int id) {
+        return getBestMovies().stream()
+                .filter(movie -> movie.getId() == id)
+                .findFirst();
+    }
+
+    private Optional<Song> getSongById(int id) {
+        return getBestSongs().stream()
+                .filter(song -> song.getId() == id)
+                .findFirst();
     }
 }
